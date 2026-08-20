ee95750cbb95dd11a591e077e92e9d7c304fb837
Aliaksei Kalosha <aliaksei.kalosha@outlook.com>
2026-08-20 09:42:34 +0200

    Add different color convert methods

--- public/ImageProcessor/imageProcessor.js
+++ public/ImageProcessor/imageProcessor.js
@@ -2,11 +2,25 @@ const image_input = document.querySelector('#image_input');
 const display = document.querySelector('#display_canvas');
 const result = document.querySelector('#result_canvas');
 const colorHolderTemplate = document.querySelector('#color_holder_temp');
-
+const conversionSelector = document.querySelector(`#method`);
+const conversionOption = {
+  "Random": convertImageRandom,
+  "Mesh": convertImageMesh,
+  "Error": convertImageError
+}
 let currentImage = null;
 let colorDataList = [];
 init();
 function init() {
+  var options = Object.keys(conversionOption);
+  conversionSelector.options = [];
+  for (let i = 0; i < options.length; i++) {
+    const x = options[i];
+    conversionSelector.options[i] = new Option(x, x);
+  }
+  conversionSelector.onchange = function () {
+    convertImage();
+  }
 
   image_input.addEventListener('change', function () {
     const file_reader = new FileReader();
@@ -14,7 +28,8 @@ function init() {
       currentImage = new Image();
       currentImage.src = file_reader.result;
       currentImage.onload = () => {
-        drawImageScaled(currentImage, display);
+        fitCanvasToImage(currentImage, display);
+        initSettings();
         fillSettings();
         convertImage();
       }
@@ -23,18 +38,35 @@ function init() {
   });
 }
 
-function drawImageScaled(img, canvas) {
+function fitCanvasToImage(img, canvas) {
   var ctx = canvas.getContext("2d");
-  var ratio = Math.min(canvas.width / img.width, canvas.height / img.height);
-  var centerShift = {
-    x: (canvas.width - img.width * ratio) / 2,
-    y: (canvas.height - img.height * ratio) / 2
-  };
+  canvas.width = img.width;
+  canvas.height = img.height;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(img, 0, 0, img.width, img.height, centerShift.x, centerShift.y, img.width * ratio, img.height * ratio);
+  ctx.drawImage(img, 0, 0);
 }
 
 function fillSettings() {
+  for (let i = 0; i < colorDataList.length; i++) {
+    let node = document.getElementById(colorHolderTemplate.id.replace("temp", i));
+    if (node) {
+      node.remove();
+    }
+    const element = colorDataList[i];
+    node = colorHolderTemplate.cloneNode(true);
+    node.id = node.id.replace("temp", i);
+    element.node = node;
+    colorHolderTemplate.parentElement.appendChild(node);
+    node.children[0].style.backgroundColor = `#${element.color.r.toString(16)}${element.color.g.toString(16)}${element.color.b.toString(16)}`;
+    node.children[1].value = `${Math.floor(element.value / 255.0)}`;
+    node.children[1].addEventListener('input', function () {
+      element.value = node.children[1].value * 2.55;
+      convertImage();
+    }, false);
+  }
+}
+
+function initSettings() {
   var ctx = display.getContext("2d");
   const imageData = ctx.getImageData(0, 0, display.width, display.height);
   const data = imageData.data;
@@ -45,24 +77,91 @@ function fillSettings() {
       colorDataList.push(createColorData(color));
     }
   }
-  console.log(colorDataList)
+  colorDataList.sort((a, b) => a.value - b.value);
 }
 
 function convertImage() {
-  drawImageScaled(currentImage, result)
+  fitCanvasToImage(currentImage, result)
   var ctx = result.getContext("2d");
   const imageData = ctx.getImageData(0, 0, result.width, result.height);
   const data = imageData.data;
+  conversionOption[conversionSelector.value](data);
+  ctx.putImageData(imageData, 0, 0);
+}
+
+function convertImageMesh(data) {
+  const ValueTo1BitColor = (colorData, i) => {
+    if (isEqual(colorData.color, colorDataList[0].color)) {
+      return 0;
+    }
+    if (isEqual(colorData.color, colorDataList[3].color)) {
+      return 255;
+    }
+    let pixelIndex = i / 4;
+    let x = Math.floor(pixelIndex % currentImage.width);
+    let y = Math.floor(pixelIndex / currentImage.width);
+    if (isEqual(colorData.color, colorDataList[1].color)) {
+      return x % 2 == y % 2 ? 255 : 0;
+    }
+    if (isEqual(colorData.color, colorDataList[2].color)) {
+      return x % 2 == 1 & y % 2 == 1 ? 0 : 255;
+    }
+  }
+
   for (let i = 0; i < data.length; i += 4) {
     let color = { r: data[i], g: data[i + 1], b: data[i + 2] };
     const colorData = colorDataList.find((a) => isEqual(a.color, color));
-    let value = colorData.value >= Math.floor(Math.random() * 255) ? 255 : 0;
-    data[i] = value; // red
-    data[i + 1] = value; // green
-    data[i + 2] = value; // blue
+    setValue(data, i, ValueTo1BitColor(colorData, i));
   }
-  ctx.putImageData(imageData, 0, 0);
 }
+
+function convertImageRandom(data) {
+  const ValueTo1BitColor = (colorData) => {
+    if (isEqual(colorData.color, colorDataList[0].color)) {
+      return 0;
+    }
+    if (isEqual(colorData.color, colorDataList[colorDataList.length - 1].color)) {
+      return 255;
+    }
+    return colorData.value >= Math.floor(Math.random() * 255) ? 255 : 0;
+  }
+
+  for (let i = 0; i < data.length; i += 4) {
+    let color = { r: data[i], g: data[i + 1], b: data[i + 2] };
+    const colorData = colorDataList.find((a) => isEqual(a.color, color));
+    setValue(data, i, ValueTo1BitColor(colorData));
+  }
+
+}
+
+function convertImageError(data) {
+  let errorDelta = 0;
+  const ValueTo1BitColorError = (colorData, errorDelta) => {
+    if (isEqual(colorData.color, colorDataList[0].color)) {
+      return [0, 0];
+    }
+    if (isEqual(colorData.color, colorDataList[colorDataList.length - 1].color)) {
+      return [255, 0];
+    }
+    var errorDelta = (colorData.value + errorDelta) - Math.floor(Math.random() * 255);
+    return [errorDelta > 0 ? 255 : 0, errorDelta];
+  }
+  for (let i = 0; i < data.length; i += 4) {
+    let color = { r: data[i], g: data[i + 1], b: data[i + 2] };
+    const colorData = colorDataList.find((a) => isEqual(a.color, color));
+    let value = 0;
+    [value, errorDelta] = ValueTo1BitColorError(colorData, errorDelta);
+    setValue(data, i, value);
+  }
+}
+
+function setValue(data, i, value) {
+  data[i] = value; // red
+  data[i + 1] = value; // green
+  data[i + 2] = value; // blue
+}
+
+
 
 const createColorData = (color) => {
   return {
@@ -90,3 +189,10 @@ const isEqual = (obj1, obj2) => {
   return keys1.every(key =>
     obj2.hasOwnProperty(key) && obj1[key] === obj2[key]);
 };
+
+function saveImage() {
+  var link = document.getElementById('save_link');
+  link.setAttribute('download', 'resultPlaydateImage.png');
+  link.setAttribute('href', result.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+  link.click();
+}
